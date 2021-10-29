6cc9afe084b480edf8bef69fa0264dc93606aeb4
furkanlebit7 <furkanlebitt7@gmail.com>
2021-10-29 14:27:34 +0300

    Javascript Odev1 Eklendi

---
+++ JAVASCRIPT/Odev1/clock.js
@@ -0,0 +1,90 @@
+let myName = prompt("Lütfen isminizi giriniz");
+document.querySelector("#myName").innerHTML = myName;
+
+function showTime() {
+  const myClock = document.querySelector("#myClock");
+  const myDate = document.querySelector("#myDate");
+  const date = new Date();
+  let sec = date.getSeconds();
+  let min = date.getMinutes();
+  let hour = date.getHours();
+  let day = date.getDate();
+  let dayName = date.getDay();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+
+  switch (dayName) {
+    case 0:
+      dayName = "Pazartesi";
+      break;
+    case 1:
+      dayName = "Salı";
+      break;
+    case 2:
+      dayName = "Çarşamba";
+      break;
+    case 3:
+      dayName = "Perşembe";
+      break;
+    case 4:
+      dayName = "Cuma";
+      break;
+    case 5:
+      dayName = "Cumartesi";
+      break;
+    case 6:
+      dayName = "Pazar";
+      break;
+  }
+
+  switch (month) {
+    case 0:
+      month = "Ocak";
+      break;
+    case 1:
+      month = "Şubat";
+      break;
+    case 2:
+      month = "Mart";
+      break;
+    case 3:
+      v = "Nisan";
+      break;
+    case 4:
+      month = "Mayıs";
+      break;
+    case 0:
+      month = "Haziran";
+      break;
+    case 0:
+      month = "Temmuz";
+      break;
+    case 0:
+      month = "Ağustos";
+      break;
+    case 0:
+      month = "Eylül";
+      break;
+    case 0:
+      month = "Ekim";
+      break;
+    case 0:
+      month = "Kasım";
+      break;
+    case 0:
+      month = "Aralık";
+      break;
+  }
+
+  sec = sec < 10 ? "0" + sec : sec;
+  min = min < 10 ? "0" + min : min;
+  hour = hour < 10 ? "0" + hour : hour;
+
+  myClock.innerHTML = hour + ":" + min + ":" + sec;
+  myDate.innerHTML =
+    day + "/" + month + "/" + year + " " + "<u>" + dayName + "</u>";
+
+  setTimeout(showTime, 1000);
+}
+
+showTime();
