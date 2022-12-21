783bf7adaddecee829af5185a22b98b12944482c
furkanlebit7 <furkanlebitt7@gmail.com>
2022-12-21 18:27:28 +0300

    (REDUX) -> Weather App : Github Link Added

--- REDUX/weather-app/src/Components/Header.js
+++ REDUX/weather-app/src/Components/Header.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { changTheme } from "../Redux/Slices/WeatherSlice";
 
-import { BsSun, BsFillMoonStarsFill } from "react-icons/bs";
+import { BsSun, BsFillMoonStarsFill, BsGithub } from "react-icons/bs";
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -24,12 +24,18 @@ const Header = () => {
   return (
     <div className="flex items-center justify-between dark:text-white text-darkMain">
       <h1 className="text-2xl">Weekly</h1>
-      <button
-        className=" text-2xl p-2 rounded-full"
-        onClick={() => handleTheme()}
-      >
-        {theme ? <BsSun /> : <BsFillMoonStarsFill />}
-      </button>
+      <div className="flex items-center justify-center text-2xl">
+        <button className="  p-2 rounded-full" onClick={() => handleTheme()}>
+          {theme ? <BsSun /> : <BsFillMoonStarsFill />}
+        </button>
+        <a
+          href="https://github.com/furkanlebit7/kodluyoruzilkrepo/tree/main/REDUX/weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsGithub className="ml-5" />
+        </a>
+      </div>
     </div>
   );
 };
