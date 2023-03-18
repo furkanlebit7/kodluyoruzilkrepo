cb298180b6f4a6f8877b362e4c4f7a69c1808322
furkanlebit7 <furkanlebitt7@gmail.com>
2023-03-18 11:50:34 +0300

    ejs added

--- NODEJS/cleanBlog/app.js
+++ NODEJS/cleanBlog/app.js
@@ -2,9 +2,25 @@ const express = require("express");
 
 const app = express();
 
+//TEMPLATE ENGINE
+app.set("view engine", "ejs");
+
+//MIDDLEWARES
+app.use(express.static("public"));
+
+//ROUTE
 app.get("/", (req, res) => {
-  res.send({ id: 1, title: "Blog title", description: "Blog description" });
-  res.end();
+  // res.sendFile(path.resolve(__dirname, "temp/index.html"));
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/add-post", (req, res) => {
+  res.render("add_post");
+});
+app.get("/post", (req, res) => {
+  res.render("post");
 });
 
 const port = 3000;
