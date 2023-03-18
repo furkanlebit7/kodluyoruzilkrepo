c2304f84b64e87129d918360956f7c9b8b973bea
furkanlebit7 <furkanlebitt7@gmail.com>
2023-03-18 11:38:46 +0300

    template engine used

--- NODEJS/pcat/app.js
+++ NODEJS/pcat/app.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const path = require("path");
+const ejs = require("ejs");
 
 const app = express();
+
+//TEMPLATE ENGINE
+app.set("view engine", "ejs");
 
 //MIDDLEWARES
 app.use(express.static("public"));
 
+//ROUTE
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+  // res.sendFile(path.resolve(__dirname, "temp/index.html"));
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/add", (req, res) => {
+  res.render("add");
+});
+app.get("/video-page", (req, res) => {
+  res.render("video-page");
 });
 
 const port = 3000;
