d62007f1b63c90dd1ecb370ed4e001ee64cf9036
furkanlebit7 <furkanlebitt7@gmail.com>
2023-03-18 09:02:47 +0300

    middleware added

---
+++ NODEJS/pcat/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+//MIDDLEWARES
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log("Project is up");
+});
