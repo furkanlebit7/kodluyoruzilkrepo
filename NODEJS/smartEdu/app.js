7ea29934f2612762e452349519ce6c271bab5126
furkanlebit7 <furkanlebitt7@gmail.com>
2023-03-20 07:43:20 +0300

    template updates

--- NODEJS/smartEdu/app.js
+++ NODEJS/smartEdu/app.js
@@ -2,8 +2,22 @@ const express = require("express");
 
 const app = express();
 
+//TEMPLATE ENGINE
+app.set("view engine", "ejs");
+
+//MIDDLEWARE
+app.use(express.static("public"));
+
+//ROUTES
 app.get("/", (req, res) => {
-  res.send("index");
+  res.status(200).render("index", {
+    page_name: "index",
+  });
+});
+app.get("/about", (req, res) => {
+  res.status(200).render("about", {
+    page_name: "about",
+  });
 });
 
 const port = 3000;
